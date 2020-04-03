1a89b38fd6e803c659affc76ea5e0c07ec42996d
Ricardo Araújo <ricardo.araujo.t0114627@edu.atec.pt>
2020-04-03 06:29:06 +0100

    Made minor tweaks

---
+++ BlazingPizza.Client/Auth/AuthStateProvider1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace BlazingPizza.Client.Auth
+{
+    public class AuthStateProvider1 : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync() 
+        {
+            var identity = new ClaimsIdentity();
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(identity)));
+        }
+    }
+}
